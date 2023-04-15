88d1e008228a239a1336c3a588a90263ba1c138e
Thịnh Nguyễn Quốc <thinhnq3009@gmail.com>
2023-04-16 00:27:17 +0700

    Phần xáo block sau mỗi n lần xuất hiện 🪨🪨🪨🪨🪨

--- obstacle.js
+++ obstacle.js
@@ -3,43 +3,59 @@ import { randomGridPosition } from "./gird.js";
 import { onSnake } from "./snake.js";
 
 let obstacles = [];
+const SHUFFLE_POSITION_EVERY = 3; 
+const MAX_OBSTACLES = 6; // -1 for infinite
+const TIME_RENDER = 2;
 
 export function update() {
-  for (const obstacle of obstacles) {
-    if (onSnake(obstacle)) {
-      return true;
+
+    for (const obstacle of obstacles) {
+        if (onSnake(obstacle)) {
+            return true;
+        }
     }
-  }
 }
 
 export function draw(gameBoard) {
-  obstacles.forEach((obstacle) => {
-    const obstacleElement = document.createElement("div");
-    obstacleElement.style.gridRowStart = obstacle.y;
-    obstacleElement.style.gridColumnStart = obstacle.x;
-    obstacleElement.classList.add("obstacle");
-    gameBoard.appendChild(obstacleElement);
-  });
+    obstacles.forEach((obstacle) => {
+        const obstacleElement = document.createElement("div");
+        obstacleElement.style.gridRowStart = obstacle.y;
+        obstacleElement.style.gridColumnStart = obstacle.x;
+        obstacleElement.classList.add("obstacle");
+        gameBoard.appendChild(obstacleElement);
+    });
 }
 
 function getRandomObstaclePosition() {
     let newObstaclePosition;
     do {
-      newObstaclePosition = randomGridPosition();
-    } while (newObstaclePosition == null || onSnake(newObstaclePosition) || isFood(newObstaclePosition));
+        newObstaclePosition = randomGridPosition();
+    } while (
+        newObstaclePosition == null ||
+        onSnake(newObstaclePosition) ||
+        isFood(newObstaclePosition)
+    );
     return newObstaclePosition;
-  }
-  
-  function isFood(position) {
+}
+
+function isFood(position) {
     return position.x === food.x && position.y === food.y;
-  }
-  
-  
+}
 
 setInterval(() => {
-  if (obstacles.length < 6) {
-    obstacles.push(getRandomObstaclePosition());
-  } else {
-    obstacles = [];
-  }
-}, 2000);
+    console.log("Render obstacle", obstacles.length);
+
+    if (obstacles.length < MAX_OBSTACLES || MAX_OBSTACLES === -1) {
+        if (obstacles.length % SHUFFLE_POSITION_EVERY === 0 && obstacles.length != 0) {
+            const length = obstacles.length;
+            obstacles = [];
+            for (let i = 0; i < length + 1; i++) {
+                obstacles.push(getRandomObstaclePosition());
+            }
+        } else {
+            obstacles.push(getRandomObstaclePosition());
+        }
+    } else {
+        obstacles = [];
+    }
+}, TIME_RENDER * 1000);
